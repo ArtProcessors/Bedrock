cf195fa1b7737bb0c852c7c6a4eaa1852aea9a13
seb.beswick@gmail.com <seb.beswick@gmail.com>
2020-05-06 16:07:07 +1000

    NSDictionary extensions: Added additional helpers, updated nullability

--- Bedrock/NSDictionary+Bedrock.h
+++ Bedrock/NSDictionary+Bedrock.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface NSDictionary<KeyType, ObjectType> (Bedrock)
+@interface NSDictionary<__covariant KeyType, __covariant ObjectType> (Bedrock)
 
 
 //
@@ -39,6 +39,52 @@ NS_ASSUME_NONNULL_BEGIN
 // take precedence.
 - (NSDictionary<KeyType, ObjectType> *)br_dictionaryByAddingEntriesFromDictionary:(NSDictionary<KeyType, ObjectType> *)dictionary;
 
+
+//
+// Copying
+//
+
+// Will return a "deep copy". When called on an NSArray or NSDictionary, it will
+// call -br_deepCopy on each object if it responds to -br_deepCopy, or -copy otherwise.
+// Note that this won't be able to deep copy array or dictionary properties on objects
+// _within_ an array or dictionary, unless the object itself implements -br_deepCopy.
+- (instancetype)br_deepCopy;
+
+
+//
+// Nested traversal
+//
+
+// <keyPath> is split by "." characters, and then each path component is used to
+// try and traverse a nested dictionary using objectForKey:. If any of the values
+// returned while traversing the nested dictionary are either nil or not an
+// NSDictionary, the method will return nil.
+- (nullable id)br_nestedObjectForKeyPath:(NSString *)keyPath;
+
+
+//
+// Parsing
+//
+
+- (nullable NSString *)br_stringForKey:(KeyType)key;
+- (nullable NSString *)br_stringForKey:(KeyType)key defaultValue:(NSString *)value;
+
+- (NSInteger)br_integerForKey:(KeyType)key;
+- (NSInteger)br_integerForKey:(KeyType)key defaultValue:(NSInteger)integer;
+
+- (BOOL)br_boolForKey:(KeyType)key;
+- (BOOL)br_boolForKey:(KeyType)key defaultValue:(BOOL)defaultValue;
+
+- (double)br_doubleForKey:(KeyType)key;
+- (double)br_doubleForKey:(KeyType)key defaultValue:(BOOL)defaultValue;
+
+- (nullable NSArray *)br_arrayForKey:(KeyType)key;
+- (nullable NSArray *)br_arrayForKey:(KeyType)key defaultValue:(NSArray *)defaultValue;
+
+- (nullable NSDictionary *)br_dictionaryForKey:(KeyType)key;
+- (nullable NSDictionary *)br_dictionaryForKey:(KeyType)key defaultValue:(NSDictionary *)defaultValue;
+
+- (nullable ObjectType)br_objectForKey:(KeyType)key ifItIsKindOfClass:(Class)class defaultValue:(nullable id)defaultValue;
 
 
 //
